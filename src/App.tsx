2bf2be84769557fd7f1a9d6dc6e9abe6aa48bb42
Paulo Vicente <vcente82@gmail.com>
2022-06-09 09:00:17 -0300

    ..

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import { Header } from "./components/Header";
